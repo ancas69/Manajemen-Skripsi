132bbc7524fbd8d62055b6a4bcda3b31d8bb24bd
ancas69 <as.ancas.sama@gmail.com>
2019-03-10 07:48:01 +0700

    complete

--- class_bs.php
+++ class_bs.php
@@ -7,7 +7,7 @@
 			$this->host = "localhost";
 			$this->user = "root";
 			$this->pass = "";
-			$this->db = "manajemen_skripsi_rpl";
+			$this->db = "manajemen_skripsi_prpl";
 		}
 		public function connect(){
 			$this->conn = mysqli_connect($this->host,$this->user,$this->pass,$this->db);
@@ -19,7 +19,7 @@
 		
 		public function show_data($jey)
 		{
-			$query = "SELECT skripsi.id_skripsi as idsk, dosen.Nama as namdos ,skripsi.judul_skripsi as judul , mahasiswa_metopen.Nama as name , mahasiswa_metopen.NIM as nim from dosen join mahasiswa_metopen on dosen.NIY = mahasiswa_metopen.Dosen join skripsi on mahasiswa_metopen.NIM = skripsi.NIM and mahasiswa_metopen.NIM = $jey ";
+			$query = "SELECT skripsi.id_skripsi as idsk, dosen.nama as namdos ,skripsi.judul_skripsi as judul , mahasiswa_metopen.nama as name , mahasiswa_metopen.nim as nim from dosen join mahasiswa_metopen on dosen.niy = mahasiswa_metopen.dosen join skripsi on mahasiswa_metopen.nim = skripsi.nim and mahasiswa_metopen.nim = $jey ";
 			$this->execute($query);
 			return $this->result;
 		}
@@ -33,7 +33,7 @@
 
 		public function select_one_mahasiswa($key)
 		{
-			$query = "SELECT *,mahasiswa_metopen.Nama as namaa, mahasiswa_metopen.NIM as Nm, dosen.Nama as namdis from logbook_bimbingan join skripsi on logbook_bimbingan.id_skripsi = skripsi.id_skripsi join mahasiswa_metopen on mahasiswa_metopen.NIM = skripsi.NIM join dosen on dosen.NIY = mahasiswa_metopen.Dosen and skripsi.NIM = $key";
+			$query = "SELECT *,mahasiswa_metopen.nama as namaa, mahasiswa_metopen.nim as Nm, dosen.nama as namdis from logbook_bimbingan join skripsi on logbook_bimbingan.id_skripsi = skripsi.id_skripsi join mahasiswa_metopen on mahasiswa_metopen.nim = skripsi.nim join dosen on dosen.niy = mahasiswa_metopen.Dosen and skripsi.nim = $key";
 			$this->execute($query);
 			return $this->result;
 		}
